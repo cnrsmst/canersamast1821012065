00cd77ebb26fcd93369c7dd980a9363951fc7262
Caner <cnr.smst@hotmail.com>
2020-12-03 20:40:23 +0300

    ikincigonderim

--- app_server/controllers/digerleri.js
+++ app_server/controllers/digerleri.js
@@ -2,7 +2,9 @@
 
 
 const hakkinda=function(req, res, next) {
-  res.render('hakkinda', { title:'Hakkında' });
+  res.render('hakkinda', { title:'Hakkında',
+  'footer': 'Caner SAMAST 2020'
+ });
 }
 
 module.exports={
